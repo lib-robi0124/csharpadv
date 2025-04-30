15f5b141e9eb3da0934956aa66c57f30b1b6fe30
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-04-30 15:45:29 +0200

    pol
    olp

---
+++ class03/StaticClasses/Polymorphism/Models/Pet.cs
@@ -0,0 +1,12 @@
+﻿namespace Polymorphism.Models
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+       
+        public virtual void Eat()
+        {
+            Console.WriteLine("wau makes a sound.");
+        }
+    }
+}
