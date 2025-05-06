c1c7738305826b43408a246bce14ca72ba8cd3d4
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-05-06 07:55:16 +0200

    dada

--- class03/class03/class03/obj/Debug/net8.0/class03.AssemblyInfo.cs
+++ class03/class03/class03/obj/Debug/net8.0/class03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("class03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97f1f2fb471c5325afd1b0c1a5b031578e80ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c6cc162cbd5ef136ba6f68c6fbd0a3bc667e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("class03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("class03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
