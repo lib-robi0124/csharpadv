5053bf4c05d3985073f06e963a871820bf281799
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-05-06 20:54:29 +0200

    uguy
    hgch

--- class03/class03/Exercise01/obj/Debug/net8.0/Exercise01.AssemblyInfo.cs
+++ class03/class03/Exercise01/obj/Debug/net8.0/Exercise01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a9df273fa16fd729c9cd66ed8dfad0109df6d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f5ae473cb740095f9cf7c23b0478d1f5fc50e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- class03/class03/Polymorphism/obj/Debug/net8.0/Polymorphism.AssemblyInfo.cs
+++ class03/class03/Polymorphism/obj/Debug/net8.0/Polymorphism.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a9df273fa16fd729c9cd66ed8dfad0109df6d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f5ae473cb740095f9cf7c23b0478d1f5fc50e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
