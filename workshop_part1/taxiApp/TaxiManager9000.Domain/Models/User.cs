115dff28fae61c8ed601b613796fd40ec5f88bcc
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-05-08 21:08:05 +0200

    works

---
+++ workshop_part1/taxiApp/TaxiManager9000.Domain/Models/User.cs
@@ -0,0 +1,24 @@
+﻿
+using TaxiManager9000.Domain.Enums;
+
+namespace TaxiManager9000.Domain.Models
+{
+    public class User : BaseEntity
+    {
+        public User() { }
+        public User(string username, string password, Role role) 
+        {
+            UserName = username;
+            Password = password;
+            Role = role;
+        }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+        public override string Print()
+        {
+            return $"{UserName} with the role {Role}";
+        }
+    }
+}
