136bc90af9622e6ae78e7619fc8bef6a19957c14
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-05-13 22:33:18 +0200

    gfdgfdggfd

---
+++ class06/class06/Qinshift.LinqMethods/Program.cs
@@ -0,0 +1,89 @@
+﻿using Qinshift.Class06.LinqMethods.Entities;
+using Qinshift.Class06.LinqMethods.Helpers;
+using Qinshift.Class06.LinqMethods;
+
+
+//Where
+
+// SQL like sintax
+
+Console.WriteLine("==WHERE==");
+IEnumerable<Student> findBobSql = from std in Database.Students where std.FirstName == "Bob" select std;
+
+
+//Lambda
+IEnumerable<Student> findBobLamda = Database.Students.Where(x => x.FirstName == "Bob");
+
+findBobSql.ToList().PrintEntities();
+findBobLamda.ToList().PrintEntities();
+
+
+//SELECT
+Console.WriteLine("==SELECT==");
+//SQL like
+
+List<string> firstNameSql = (from std in Database.Students select std.FirstName).ToList();
+
+firstNameSql.PrintSimple();
+List<string> studentFullNamesSql = (from std in Database.Students select $"{std.FirstName} {std.LastName}").ToList();
+studentFullNamesSql.PrintSimple();
+
+//Lambda
+
+List<string> firsttNamesLinq = Database.Students.Select(x => x.FirstName).ToList();
+firsttNamesLinq.PrintSimple();
+//List<string> studentDullNameLinq = Database.Students.Select(x => $"{std.FirstName} {std.LastName}").ToList();
+
+//all partTime students on programming
+
+List<Student> progrStudentsSql = (from std in Database.Students where std.IsPartTime && std.Subjects is not null
+                                  where (from sub in std.Subjects where sub.Type == Academy.Programming
+                                         select sub).ToList().Count != 0 select std).ToList();
+
+progrStudentsSql.PrintEntities();
+
+
+//Linq
+List<Student> paartTimeProgrammingStudentsLinq = Database.Students.Where((x) => x.IsPartTime && x.Subjects is not null)
+                                                .Where(x => x.Subjects.Where(y => y.Type == Academy.Programming).ToList().Count != 0).ToList();
+
+
+paartTimeProgrammingStudentsLinq.PrintEntities();
+
+//First / Single / Last / OrDefault
+
+Console.WriteLine("==FIRST / Single / Last / OrDefault ==");
+
+
+List<string> emptyListOfStrings = new();
+List<Student> emptyListOfStudent = new();
+
+Console.WriteLine(Database.Students.First().Info());
+Console.WriteLine(Database.Students.FirstOrDefault(x => x.FirstName.StartsWith('T')).Info());
+
+Console.WriteLine(emptyListOfStudent.FirstOrDefault());
+
+//Console.WriteLine(Database.Students.Single(x => x.Age == 22).Info()); // error exception - not single
+Console.WriteLine(Database.Students.SingleOrDefault(x => x.Id == 12)?.Info());
+
+
+//Select many
+Console.WriteLine("==SELECT Many ==");
+
+//select list of all students parttime their subjects
+
+var partTimeStudentsAllSubjects = Database.Students.Where(x => x.IsPartTime && x.Subjects is not null)
+                                    .SelectMany(x => x.Subjects).ToList(); //SelectMany is creating FROM List<List<any>> TO List<Many>
+
+partTimeStudentsAllSubjects.PrintEntities();
+
+var partTimeStudentsAllSubjectsDisting = partTimeStudentsAllSubjects.Distinct().ToList();
+
+Console.WriteLine("==after dinsting ==");
+
+partTimeStudentsAllSubjectsDisting.PrintEntities();
+
+
+
+
+
---
+++ class06/class06/class06/Program.cs
@@ -0,0 +1,89 @@
+﻿List<string> names = new() { "Ada", "Sara", "Toska", "Robert" };
+int Sum(int x, int y)
+{
+    return x + y;
+}
+
+int Diff(int x, int y)
+{
+    return x - y;
+}
+
+int Multiply(int x, int y)
+{
+    return x * y;
+}
+
+#region Func
+
+Func<int, int, int> sumOftwo = (x, y) => x + y;
+Func<int, int, int> diffOfTwo = (x, y) => x - y;
+Func<int, int, int> mulOfTwo = (x, y) => x * y;
+
+Func<int, int, bool> checkIfLarger = (x, y) =>
+{
+    if (x > y)
+        return true;
+    return false;
+};
+
+Console.WriteLine($"The sum of 5 and 10 is {sumOftwo(5, 10)}");
+Console.WriteLine($"The difference of 10 and 4 is {sumOftwo(10, 4)}");
+Console.WriteLine($"The product of 123 and 10 is {sumOftwo(123, 10)}");
+if (checkIfLarger(10, 20))
+{
+    Console.WriteLine("10 is larger");
+}
+else
+{
+    Console.WriteLine("20 is larger");
+}
+
+
+
+Func<List<string>, bool> isListEmpty = (list) => list.Count == 0;
+
+Console.WriteLine($"Is The list of names is empty {isListEmpty(names)}");
+
+
+
+#endregion
+
+Console.WriteLine("====== ACTION ======");
+#region action
+
+Action hello = () => Console.WriteLine("Hello there!!");
+
+hello();
+
+
+Action<string> printInRed = (text) =>
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(text);
+    Console.ResetColor();
+};
+printInRed("This will be print in Red");
+
+Action<string, ConsoleColor> printInColor = (text, color) =>
+{
+    Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(text);
+    Console.ResetColor();
+};
+
+#endregion
+
+Console.WriteLine("==== H Order ===");
+#region Higher Order func use
+
+string foundToska = names.Find(x => x == "Toska");
+Console.WriteLine($"{foundToska}");
+
+Predicate<string> isRobert = x => x == "Robert";
+
+string foundRobert = names.Find(isRobert);
+Console.WriteLine(foundRobert);
+
+
+#endregion
+
