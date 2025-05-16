25a16a12c30d8bad90168e4fbca6678adedbad91
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-05-16 21:01:50 +0200

    hm-06

--- class06/class06/HomeworkCars/Program.cs
+++ class06/class06/HomeworkCars/Program.cs
@@ -98,3 +98,61 @@ Console.WriteLine($"Lowest Miles per galon is {lowestMilesPerGalonLinq}");
 Console.WriteLine($"Highest Miles per galon is {highestMilesPerGalonLinq}");
 Console.WriteLine($"Average Miles per galon is {averageMilesPerGalonLinq}");
 Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Order cars by their AccelerationTime in ascending order. Take the top 3 cars from the ordered list");
+List<Car> topThreeCarsByAccelerationTimeSql = (from c in DataBase.Cars
+                                               orderby c.AccelerationTime ascending
+                                               select c).Take(3).ToList();
+List<Car> topThreeCarsByAccelerationTimeLinq = DataBase.Cars
+                                                    .OrderBy(c => c.AccelerationTime)
+                                                    .Take(3)
+                                                    .ToList();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("print from like Sql topThreeCarsByAccelerationTimeSql");
+topThreeCarsByAccelerationTimeSql.PrintEntities();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("print from linq topThreeCarsByAccelerationTimeLinq");
+topThreeCarsByAccelerationTimeLinq.PrintEntities();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Filter cars that have more than 6 cylinders. Calculate the total weight of these cars." +
+                " Print the total weight.");
+List<Car> carsWithMoreThanSixCylindersForWeightSql = (from c in DataBase.Cars
+                                                      where c.Cylinders > 6
+                                                      select c).ToList();
+List<Car> carsWithMoreThanSixCylindersForWeightLinq = DataBase.Cars
+                                                            .Where(c => c.Cylinders > 6)
+                                                            .ToList();
+double totalWeightSql = carsWithMoreThanSixCylindersForWeightSql.Sum(c => c.Weight);
+double totalWeightLinq = carsWithMoreThanSixCylindersForWeightLinq.Sum(c => c.Weight);
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("print from like Sql totalWeightSql");
+Console.WriteLine($"Total weight of cars with more than 6 cylinders is {totalWeightSql}");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("print from linq totalWeightLinq");
+Console.WriteLine($"Total weight of cars with more than 6 cylinders is {totalWeightLinq}");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Filter cars that have an even number of cylinders. Calculate the average MilesPerGalon for these cars." +
+                " Print the average MilesPerGalon");
+List<Car> carsWithEvenCylindersSql = (from c in DataBase.Cars
+                                      where c.Cylinders % 2 == 0
+                                      select c).ToList();
+List<Car> carsWithEvenCylindersLinq = DataBase.Cars
+                                            .Where(c => c.Cylinders % 2 == 0)
+                                            .ToList();
+double averageMilesPerGalonEvenCylindersSql = carsWithEvenCylindersSql.Average(c => c.MilesPerGalon);
+double averageMilesPerGalonEvenCylindersLinq = carsWithEvenCylindersLinq.Average(c => c.MilesPerGalon);
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("print from like Sql averageMilesPerGalonEvenCylindersSql");
+Console.WriteLine($"Average Miles per galon of cars with even cylinders is {averageMilesPerGalonEvenCylindersSql}");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("print from linq averageMilesPerGalonEvenCylindersLinq");
+Console.WriteLine($"Average Miles per galon of cars with even cylinders is {averageMilesPerGalonEvenCylindersLinq}");
+Console.ResetColor();
