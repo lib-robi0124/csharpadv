a04aea2b6ff65e3593ff87cf24a431bb1a9c405b
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-05-20 11:07:16 +0200

    asdczx

---
+++ class07/Delegates/Events/Entites/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Events.Entites
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public bool HasWokeUp { get; set; } = false;
+        public void WakeUp()
+        {
+            Console.WriteLine($"{Name} waaaaaaake uup!!!");
+            HasWokeUp = true;
+        }
+    }
+}
