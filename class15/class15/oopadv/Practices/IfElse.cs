23bf8e89cd17d748054a331e4b9e9bceb76325de
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-06-06 19:29:02 +0200

    cs15

---
+++ class15/class15/oopadv/Practices/IfElse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopadv.Practices
+{
+    public class IfElse
+    {
+        public void IfElseExample(int num1, int num2)
+        {
+            //BAD example
+            if (num1 <= 100 && num2 <= 100)
+            {
+                if (num1 % 2 == 0 && num2 % 2 == 0)
+                {
+                    if (num1 == num2)
+                    {
+                        Console.WriteLine("they are equal");
+                    }
+                }
+            }
+
+            //GOOD example
+            if (num1 > 100 && num2 > 100) return;
+            if (num1 % 2 != 0 && num2 % 2 != 0) return;
+            if (num1 != num2) return;
+            Console.WriteLine("they are equal");
+        }
+    }
+}
---
+++ class15/class15/oopadv/Practices/Loop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopadv.Practices
+{
+    public class Loop
+    {
+        List<string> stringNames = new List<string>() { "Bob", "Jordan", "Zoran", "Goran", "Horvat", "Jill" };
+        public void LoopExample()
+        {
+            //BAD example
+            for (int i = 0; i < stringNames.Count; i++)
+            {
+                if (stringNames[i].Length == stringNames.Count)
+                {
+                    Console.WriteLine(stringNames[i]);
+                }
+            }
+
+            //GOOD example
+            foreach (string name in stringNames)
+            {
+                if (name.Length == stringNames.Count)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+
+    }
+}
